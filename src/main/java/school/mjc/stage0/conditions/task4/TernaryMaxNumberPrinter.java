609de0f2e8003c5cf20cc369f5e479f8ebac70d9
Baimenovbn <baibn@mail.ru>
2022-12-23 11:10:11 +0600

    feat: finish task

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int biggestFromTwo = first > second ? first : second;
+        System.out.println(biggestFromTwo > third ? biggestFromTwo : third);
     }
 }
